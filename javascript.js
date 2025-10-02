be969a0665a50170c1b48c4f3687cfad5e309754
Myogi <myogipandi@gmail.com>
2025-10-02 23:23:34 +0530

    Trying to commit

---
+++ javascript.js
@@ -0,0 +1,15 @@
+
+function signup() {
+  document.getElementById("up").style.display = "block";
+  document.getElementById("log").style.display = "none";
+}
+
+function login() {
+  document.getElementById("log").style.display = "block";
+  document.getElementById("up").style.display = "none";
+}
+
+function clos() {
+  document.getElementById("up").style.display = "none";
+  document.getElementById("log").style.display = "none";
+}
